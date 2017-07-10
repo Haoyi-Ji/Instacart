63583aa91fefaecb51d013c87ea7e4e375440fa5
howie <haoyi.ji@mljr.com>
2017-07-10 12:05:17 +0800

    fix gbdt_lr.py

--- model/gbdt_lr.py
+++ model/gbdt_lr.py
@@ -8,11 +8,11 @@ from sklearn.linear_model import LogisticRegression
 
 
 
-predicted_prob = lr.predict_prob(X_test)
+predicted_prob = lr.predict_proba(X_test)
 
 
-def calc_threshold(predicted_prob, y_true, step=0.05, beta=1):
-    predicted_prob = predicted_prob.T[lr.classes_.argmax()].T
+def calc_threshold(predicted_prob, y_true, lr_model, step=0.05, beta=1):
+    predicted_prob = predicted_prob.T[lr_model.classes_.argmax()].T
     predicted_prob = pd.DataFrame(predicted_prob, columns=['proba'])
     predicted_prob['y_true'] = y_true.reset_index().drop('index', axis=1)
 
@@ -20,9 +20,11 @@ def calc_threshold(predicted_prob, y_true, step=0.05, beta=1):
     max_f_score = 0
     max_threshold = threshold
     while threshold < 1:
+        print('Threshold = {}'.format(threshold))
         predicted_prob['y_pred'] = predicted_prob.proba.apply(lambda x: 1 if x > threshold else 0)
         double_one_cnt = len(predicted_prob[predicted_prob.y_pred+predicted_prob.y_true==2])
         if double_one_cnt == 0:
+            threshold += step
             continue
         p = double_one_cnt / predicted_prob.y_pred.sum()
         r = double_one_cnt / predicted_prob.y_true.sum()
